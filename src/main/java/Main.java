a3c7b08a05adfb9a00cbda135863341f261673de
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 16:00:41 -0400

    good inputs work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        int numInput;
+        int sum = 0;
 
+        //prompt user for integer 1 or greater
+        System.out.print("Choose an integer greater than 0: ");
+        Scanner in = new Scanner(System.in);
+        numInput = Integer.parseInt(in.next());
+
+        // calculate sum from 1 to n
+        for (int i = 1; i <= numInput; i++) {
+            sum += i;
+        }
+
+        //return sum of 1 to n
+        System.out.println("Your sum from 1 to " + numInput + " is: " + sum);
     }
 }
