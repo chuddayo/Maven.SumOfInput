fa50a7d08321b96ab654154cf79d3e99e4ffc0a0
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 16:26:31 -0400

    added exception carry

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,20 +6,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        int numInput;
+        int numInput = 0;
         int sum = 0;
 
         //prompt user for integer 1 or greater
         System.out.print("Choose an integer greater than 0: ");
         Scanner in = new Scanner(System.in);
-        numInput = Integer.parseInt(in.next());
 
-        // calculate sum from 1 to n
-        for (int i = 1; i <= numInput; i++) {
-            sum += i;
+        try {
+            numInput = Integer.parseInt(in.next());
+
+            // calculate sum from 1 to n
+            for (int i = 1; i <= numInput; i++) {
+                sum += i;
+            }
+
+            //return sum of 1 to n
+            System.out.println("Your sum from 1 to " + numInput + " is: " + sum);
+        } catch (Exception e) {
+            System.out.println("Something went wrong. check again");
         }
-
-        //return sum of 1 to n
-        System.out.println("Your sum from 1 to " + numInput + " is: " + sum);
     }
 }
